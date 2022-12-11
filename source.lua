c4d4ba6e3b98a1488930a54a6107da32824ce552
JohnyJonson <118004367+JohnyJonson@users.noreply.github.com>
2022-12-11 11:46:41 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -34,6 +34,41 @@ Tab:AddTextbox({
 })
 task.wait(2)
 OrionLib:Destroy()
+	local MainOrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+				local Window = OrionLib:MakeWindow({Name = "Private Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+				local Tab = Window:MakeTab({
+	Name = "Main (OP)",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "Kill youreself (works in all games and games with reset button off)",
+	Callback = function()
+      		game.Players.LocalPlayer.Humanoid.Health = 0
+  	end    
+})
+
+--[[
+Name = <string> - The name of the button.
+Callback = <function> - The function of the button.
+]]
+--[[
+Name = <string> - The name of the tab.
+Icon = <string> - The icon of the tab.
+PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
+]]
+
+--[[
+Name = <string> - The name of the UI.
+HidePremium = <bool> - Whether or not the user details shows Premium status or not.
+SaveConfig = <bool> - Toggles the config saving in the UI.
+ConfigFolder = <string> - The name of the folder where the configs are saved.
+IntroEnabled = <bool> - Whether or not to show the intro animation.
+IntroText = <string> - Text to show in the intro animation.
+IntroIcon = <string> - URL to the image you want to use in the intro animation.
+Icon = <string> - URL to the image you want displayed on the window.
+CloseCallback = <function> - Function to execute when the window is closed.
+]]
 --[[
 Title = <string> - The title of the notification.
 Content = <string> - The content of the notification.
